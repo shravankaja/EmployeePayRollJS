3b7960aeff1ef50e9853fbfd2e3322e7f7c32fcc
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-09 14:26:46 +0530

    Added: two more constants

---
+++ Constants.js
@@ -0,0 +1,7 @@
+let fullTimeEmployeeHours = 8;
+let fullTimeEmployeePay   = 20;
+let partTimeEmployeeHours = 4;
+let partTimeEmployeePay = 8;
+module.exports = { fullTimeEmployeeHours , fullTimeEmployeePay, 
+                    
+                    partTimeEmployeeHours, partTimeEmployeePay    }
