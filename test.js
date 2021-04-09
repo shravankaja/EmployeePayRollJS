f31d84abb2ddcb01583b85e309bc0a128ac4dd0c
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-09 13:59:17 +0530

    Added: test case for calculatedailywage

--- test.js
+++ test.js
@@ -6,7 +6,13 @@ const employeePayRollSystem = new EmployeePayRollSystem();
 describe("Testin Employee Pay Roll System",() => {
     
     it("Test check Attendence",()=> {
-        const result = employeePayRollSystem.checkAttandence();
-        expect(result).to.be.true;
+        const result = employeePayRollSystem.checkPresentOrNot();
+        expect(result).to.be.a('boolean')
+    })
+
+    it("Test calculate daily wage method", () => {
+        const pay = employeePayRollSystem.calculateDailyWage();
+        expect(pay).to.be.a('number')
+        expect(pay).to.be.equal(160)
     })
 });
