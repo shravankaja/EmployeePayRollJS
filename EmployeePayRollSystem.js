fc378ab5452025417351379d02e6adf9ddad1220
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-09 20:05:03 +0530

    Added: method to store dailyWage and monthly wage of an employee

--- EmployeePayRollSystem.js
+++ EmployeePayRollSystem.js
@@ -3,6 +3,9 @@ const constants = require('./Constants')
 const EmployeeBuilder = require('./Employee')
 class EmployeePayRollSystem {
 
+    constructor() {
+        let map = new Map();
+    }
     checkPresentOrNot() {
         let randomNumber = Math.round(Math.random())
         if (randomNumber == 1) {
@@ -54,16 +57,16 @@ class EmployeePayRollSystem {
         let result = this.simulateTwentyDaysAttandence();
         let type = obj.type
         if (type == "Full") {
-            if (result[1] > 15 || result[2] > 100) {
-                return true
+            if (result[0] > 10 || result[2] > 80) {
+                return true;
             }
             else {
                 return false;
             }
         }
         else if (type == "Part") {
-            if (result[1] > 10 || result[2] > 80) {
-                return true
+            if (result[0] > 10 || result[2] > 80) {
+                return true;
             }
             else {
                 return false;
@@ -72,16 +75,27 @@ class EmployeePayRollSystem {
 
     }
 
+    storeDailyWageAndDailyWage(name, dailyWage, TotalWage) {
+        let empSalary = new Map();
+        empSalary.set('name', name);
+        empSalary.set('dailyWage', dailyWage)
+        empSalary.set('TotalWage', TotalWage)
+        return empSalary
+    }
+
     calculateMonthlyWage(obj) {
         let payEligibilty = this.checkSalaryEligibilty(obj);
+        let map;
         if (payEligibilty == true) {
             let result = this.simulateTwentyDaysAttandence()
             let dailyWage = this.calculateWageOFEmployee(obj)
-            return dailyWage * result[0];
+            let result1 = dailyWage * result[0];
+            map = this.storeDailyWageAndDailyWage(obj.name, dailyWage, result1)
+            return [result1, map];
         }
         else {
             console.log("Employee has not met condition to get salary this month")
-            return 0;
+            return [0, map];
         }
     }
 }
