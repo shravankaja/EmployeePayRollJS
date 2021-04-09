331df4d27ebf2c35baacdf57f8f971f00162d049
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-09 15:01:29 +0530

    Added: method to calucate dailywage of employee based on full time or part time

--- EmployeePayRollSystem.js
+++ EmployeePayRollSystem.js
@@ -1,21 +1,40 @@
 console.log("Welcome to Employee Pay Roll System ")
 const constants = require('./Constants')
+const EmployeeBuilder = require('./Employee')
 class EmployeePayRollSystem {
 
-    checkPresentOrNot () {
+    checkPresentOrNot() {
         let randomNumber = Math.round(Math.random())
-        if(randomNumber == 1) {
+        if (randomNumber == 1) {
             return true;
         }
-        else if(randomNumber == 0) {
+        else if (randomNumber == 0) {
             return false;
         }
     };
 
-    calculateDailyWage () {
-        return constants.fullTimeEmployeeHours*constants.fullTimeEmployeePay;
-
+    calculateDailyWageFullTime() {
+        return constants.fullTimeEmployeeHours * constants.fullTimeEmployeePay;
     }
+
+    calculateDailyWagePartTime() {
+        return constants.partTimeEmployeeHours * constants.partTimeEmployeePay;
+    }
+
+    calculateWageOFEmployee(obj) {
+        const type = obj.type;
+        switch (type) {
+            case "Full":
+                return this.calculateDailyWageFullTime();
+
+            case "Part":
+                return this.calculateDailyWagePartTime();
+
+            default:
+                return 0;
+        }
+    }
+
 }
 
 module.exports = EmployeePayRollSystem;
