2060a247755d4681a84403d5aae91fd9bfdfb05b
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-09 13:35:37 +0530

    Added: method to check if employee is  present or not

--- EmployeePayRollSystem.js
+++ EmployeePayRollSystem.js
@@ -1,1 +1,16 @@
-console.log("Welcome to Employee Pay Roll System    ")
+console.log("Welcome to Employee Pay Roll System ")
+
+class EmployeePayRollSystem {
+
+    checkAttandence () {
+        let randomNumber = Math.round(Math.random())
+        if(randomNumber == 1) {
+            return true;
+        }
+        else if(randomNumber == 0) {
+            return false;
+        }
+    };
+}
+
+module.exports = EmployeePayRollSystem;
