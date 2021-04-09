5f3bfaae12b3c1f6185d634256d00b4ef7d700a9
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-09 14:28:35 +0530

    Added: test case for part time

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 var expect = require('chai').expect
 const EmployeePayRollSystem = require('./EmployeePayRollSystem')
 const employeePayRollSystem = new EmployeePayRollSystem();
+const EmployeeBuilder = require('./Employee')
 
 
-describe("Testin Employee Pay Roll System",() => {
-    
-    it("Test check Attendence",()=> {
+describe("Testin Employee Pay Roll System", () => {
+
+    it("Test check Attendence", () => {
         const result = employeePayRollSystem.checkPresentOrNot();
         expect(result).to.be.a('boolean')
     })
@@ -14,5 +15,13 @@ describe("Testin Employee Pay Roll System",() => {
         const pay = employeePayRollSystem.calculateDailyWage();
         expect(pay).to.be.a('number')
         expect(pay).to.be.equal(160)
+    })
+
+    it("To check is system is able to differntiate between full time and part time employees ", () => {
+
+        const partTimeEmployee = new EmployeeBuilder("Shravan", 123).setEmployeeType("Part").build();
+        expect(partTimeEmployee.type).to.be.equal("Part");
+        const fullTimeEmployee = new EmployeeBuilder("Shravan", 123).setEmployeeType("Full").build();
+        expect(fullTimeEmployee.type).to.be.equal("Full");
     })
 });
