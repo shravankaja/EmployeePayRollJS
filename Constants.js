1faa53b54eef2ca88950facde2d3574dbdfe1c6b
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-09 15:25:05 +0530

    Refactored: inentation

--- Constants.js
+++ Constants.js
@@ -1,7 +1,9 @@
 let fullTimeEmployeeHours = 8;
-let fullTimeEmployeePay   = 20;
+let fullTimeEmployeePay = 20;
 let partTimeEmployeeHours = 4;
 let partTimeEmployeePay = 8;
-module.exports = { fullTimeEmployeeHours , fullTimeEmployeePay, 
-                    
-                    partTimeEmployeeHours, partTimeEmployeePay    }
+
+module.exports = {
+    fullTimeEmployeeHours, fullTimeEmployeePay,
+    partTimeEmployeeHours, partTimeEmployeePay
+}
