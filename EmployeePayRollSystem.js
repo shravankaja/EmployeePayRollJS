f195f8f751be37b4d88cc0fce125837d3bb9639b
shravankaja <60179805+shravankaja@users.noreply.github.com>
2021-04-15 08:02:44 +0530

    Reactored: delegated task which are not meant to be in this class

--- EmployeePayRollSystem.js
+++ EmployeePayRollSystem.js
@@ -1,101 +1,45 @@
 console.log("Welcome to Employee Pay Roll System ")
 const constants = require('./Constants')
 const EmployeeBuilder = require('./Employee')
+const Randomization = require('./Randomization')
+const Compute = require('./Compute')
+const Simulate = require('./Simulate')
+const Store = require('./Store')
+const store = new Store();
+const simulate = new Simulate();
+const compute = new Compute();
+const random = new Randomization();
 class EmployeePayRollSystem {
-
-    checkPresentOrNot() {
-        let randomNumber = Math.round(Math.random())
-        if (randomNumber == 1) {
-            return true;
-        }
-        else if (randomNumber == 0) {
-            return false;
-        }
-    };
-
-    calculateDailyWageFullTime() {
-        return constants.fullTimeEmployeeHours * constants.fullTimeEmployeePay;
-    }
-
-    calculateDailyWagePartTime() {
-        return constants.partTimeEmployeeHours * constants.partTimeEmployeePay;
-    }
-
-    calculateWageOFEmployee(obj) {
-        let type1 = obj.type;
-        switch (type1) {
-            case "Full":
-                return this.calculateDailyWageFullTime();
-
-            case "Part":
-                return this.calculateDailyWagePartTime();
-
-            default:
-                return 0;
-        }
-    }
-
-    simulateTwentyDaysAttandence(obj) {
-        let map = new Map()
-        let noOfDaysPersent = 0;
-        let noOfDaysAbsent = 0;
-        let day;
-        for (let i = 0; i <= 20; i++) {
-            let result = this.checkPresentOrNot();
-            if (result == true) {
-                noOfDaysPersent += 1
-                day = "day" + i;
-                map.set(day, this.calculateWageOFEmployee(obj))
-            }
-            else if (result == false) {
-                noOfDaysAbsent += 1
-                day = "day" + i;
-                map.set(day, 0)
-            }
-        }
-        return [noOfDaysPersent, noOfDaysAbsent, noOfDaysPersent * 8, noOfDaysPersent * 4, map]
-    }
-
+    
     checkSalaryEligibilty(obj) {
-        let result = this.simulateTwentyDaysAttandence(obj);
+        let result = simulate.simulateDays(20,obj)
         let type = obj.type
-        if (type == "Full") {
-            if (result[0] > 1 || result[2] > 10) {
-                return true;
-            }
-            else {
-                return false;
-            }
+        if (result[0]!=0) {
+            if (result[0] > 13 || result[2] > 95) return true;
+            else return false;
         }
-        else if (type == "Part") {
-            if (result[0] > 10 || result[2] > 80) {
-                return true;
-            }
-            else {
-                return false;
-            }
+        else if (result[0]!=0) {
+            if (result[0] > 13 || result[2] > 45) return true;
+             else return false;
         }
-
     }
-
-    storeDailyWageAndDailyWage(name1, dailyWage, TotalWage, map) {
-        let empSalary = new Map();
-        empSalary.set('name', name1);
-        empSalary.set('dailyWage', dailyWage)
-        empSalary.set('TotalWage', TotalWage)
-        empSalary.set('DayWiseWage', map)
-        return empSalary
+    
+    selectFullTimeOrPartTime(name,id) {
+        let result = random.getBoolean();
+        let emp;
+        if(result == true ) return  emp = new EmployeeBuilder(name,id).build();
+        else if(result == false )  return  emp = new EmployeeBuilder(name,id).build();
     }
-
+    
     calculateMonthlyWage(obj) {
         let payEligibilty = this.checkSalaryEligibilty(obj);
         let map;
         if (payEligibilty == true) {
-            let result = this.simulateTwentyDaysAttandence(obj)
-            let dailyWage = this.calculateWageOFEmployee(obj)
+            let result = simulate.simulateDays(20,obj)
+            let dailyWage = compute.calculateWageOFEmployee(obj)
             let result1 = dailyWage * result[0];
-            map = this.storeDailyWageAndDailyWage(obj.name, dailyWage, result1, result[4])
-            console.log(map)
+            map = store.storeDailyWageAndDailyWage(obj.name, dailyWage, result[5], result[4])
+           // console.log(map)
             return [result1, map];
         }
         else {
@@ -104,5 +48,11 @@ class EmployeePayRollSystem {
         }
     }
 }
+
+let employeePayRollSystem = new  EmployeePayRollSystem();
+let emp = employeePayRollSystem.selectFullTimeOrPartTime("Shravna",1123);
+console.log(emp)
+const result = employeePayRollSystem.calculateMonthlyWage(emp);
+console.log(result)
 
 module.exports = EmployeePayRollSystem;
